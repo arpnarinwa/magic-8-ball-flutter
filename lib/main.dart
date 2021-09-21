ffc7a443492d737acc2b80931b8c5bfa6269289e
Arpna <arpnabrekan111@gmail.com>
2021-09-21 16:56:51 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,59 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
+      const MaterialApp(
+        home: MagicBallPage()
       ),
     );
+    class MagicBallPage extends StatelessWidget {
+      const MagicBallPage({ Key? key }) : super(key: key);
+    
+      @override
+      Widget build(BuildContext context) {
+        return 
+        Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: const Text('Ask Me Anything'),
+            backgroundColor: Colors.blue[900],
+          ),
+          body: const MagicBall(), 
+        );
+      }
+    }
+    class MagicBall extends StatefulWidget {
+      const MagicBall({ Key? key }) : super(key: key);
+    
+      @override
+      _MagicBallState createState() => _MagicBallState();
+    }
+    
+    class _MagicBallState extends State<MagicBall> {
+    int ballNumber=1 ;
+    void randomBall(){
+          setState(() {
+          ballNumber=Random().nextInt(4) + 1 ;
+        },
+      );
+      
+    }
+
+      @override
+      Widget build(BuildContext context) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+             children: [
+               Expanded(
+                 child:TextButton(
+                   onPressed: randomBall,
+                   child: Image.asset(
+                     'images/ball$ballNumber.png'
+                     ),
+                 ),
+               ),
+             ]
+        );
+      }
+    }
+
